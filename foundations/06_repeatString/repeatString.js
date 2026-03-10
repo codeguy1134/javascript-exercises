e0052d868d1decd1d6be096753763f2739bfd4f0
Chris <2020386+codeguy1134@users.noreply.github.com>
2026-03-10 03:28:34 -0400

    Completed 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, a) {
+  if (a < 0) {
+    return 'ERROR';
+  }
 
-};
+  let result = '';
+
+  for (let n = 0; n < a; n++) {
+    result += string;
+  }
+
+  return result;
+}
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
