6f1eca1d82b5d451156a3bd480fc526f85129cee
chris <2020386+codeguy1134@users.noreply.github.com>
2026-05-06 21:21:17 -0400

    working thru removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    const result = arr.filter(item => !args.includes(item));
+    return result;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 7, tacos));
 
 // Do not edit below this line
 module.exports = removeFromArray;
