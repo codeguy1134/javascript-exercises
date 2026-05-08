c4a681289ae853028ed5333462734c7f60e629b2
chris <2020386+codeguy1134@users.noreply.github.com>
2026-05-08 01:11:27 -0400

    working thru 10

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
 
+const sumAll = function (a, b) {
+    const min = Math.min(a, b);
+    const max = Math.max(a, b);
+    let sum = 0;
+
+
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    } else {
+        for (let i = min; i <= max; i++) {
+            sum += i;
+        }
+    return sum;
+    }
 };
+
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
