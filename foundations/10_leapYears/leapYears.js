628d4c0625a32fe515040f658cd79e93327591c2
chris <2020386+codeguy1134@users.noreply.github.com>
2026-05-08 23:51:03 -0400

    Completed 10_leapYears

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,24 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    if (year % 4 === 0 && (year % 100 != 0 || year % 400 === 0)) {
+        return true;
+
+    } else {
+        return false;
+    }
 };
+
+console.log(leapYears(1982));
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+// THIS IS ALSO VALID CODE FOR THE EXERCISE
+
+/* 
+const leapYears = function(year) {
+    return year % 4 === 0 && (year % 100 != 0 || year % 400 === 0);
+};
+
+ */
